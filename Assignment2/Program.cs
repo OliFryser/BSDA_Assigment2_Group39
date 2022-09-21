125480001151824474bff3f8827f88bdecb26054
bhagelin1999 <95022757+bhagelin1999@users.noreply.github.com>
2022-09-21 16:47:12 +0200

    Add method for IsSecure and WordCount with tests (#5)

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,4 +1,7 @@
-﻿foreach (var wizard in WizardCollection.Create())
+﻿using System.Text.RegularExpressions;
+
+
+foreach (var wizard in WizardCollection.Create())
 {
     Console.WriteLine(wizard);
 }
