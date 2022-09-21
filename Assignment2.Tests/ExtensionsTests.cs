cbf049c32acd2e9150d598d7351814aa7513214a
Benjamin Edon Hagelin <bhag@itu.dk>
2022-09-21 16:45:54 +0200

    Add method for IsSecure and WordCount with tests

--- Assignment2.Tests/ExtensionsTests.cs
+++ Assignment2.Tests/ExtensionsTests.cs
@@ -1,10 +1,58 @@
 namespace Assignment2.Tests;
+using FluentAssertions;
+using Xunit;
+using System.Collections;
+using System.Text;
 
 public class ExtensionsTests
 {
     [Fact]
-    public void Test1()
+    public void return_true_when_given_secure_uri()
     {
+        // Given
+        var str = "s-http://youtube.com";
+        
+        
+        // When
+        var output = Extensions.IsSecure(str);
+    
 
+        // Then
+        
+        output.Should().Be(true);
+        
+    }
+
+    [Fact]
+    public void return_false_when_given_not_secure_uri()
+    {
+        // Given
+        var str = "http://s-http://youtube.com";
+        
+        
+        // When
+        var output = Extensions.IsSecure(str);
+    
+
+        // Then
+        
+        output.Should().Be(false);
+        
+    }
+
+    [Fact]
+    public void return_wordcount_given_string_with_words_and_symbols()
+    {
+        // Given
+        var str = "Hi!. my name........... is,,,,,!!!?? Benjamin !!yes.maybe,true";
+        
+        
+        // When
+        var output = Extensions.WordCount(str);
+    
+
+        // Then
+        output.Should().Be(8);
+        
     }
 }
--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -1,5 +1,18 @@
+using System.Text.RegularExpressions;
+
 namespace Assignment2;
+
 
 public static class Extensions
 {
+
+public static bool IsSecure(this string str) => Regex.IsMatch(str, "(?i)^(s-http://).*");
+  
+
+public static int WordCount(this string str) => Regex.Split(str, @"\P{L}+").Length;
+
+
+
+
+
 }
